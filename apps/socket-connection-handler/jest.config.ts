5c22116924caab86b4b3616eae96c141d24e6dde
TahaDgn <mtahadogan.biz+github@outlook.com>
2022-07-25 09:43:51 +0300

    initial commit

---
+++ apps/socket-connection-handler/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'socket-connection-handler',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/socket-connection-handler',
+};
